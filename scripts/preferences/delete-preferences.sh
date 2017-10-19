9c282f09107553d953b8264fd26335c14cdf385d
tacostacostacos <david.epstein@libertymutual.com>
2017-10-19 12:09:08 -0400

    Created delete, modified update scripts
    Created the delete script
    Removed user changes from update

--- scripts/preferences/delete-preferences.sh
+++ scripts/preferences/delete-preferences.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/preferences/"
-TOKEN="4607ef3d7e46c7620b2c858c722c5566"
-LOCATION="03878"
-SEARCH_RADIUS=99772
-# USER_ID=1
 PREFERENCE_ID=1
+TOKEN="4607ef3d7e46c7620b2c858c722c5566"
+# USER_ID=1
 
 
 curl "${API}${URL_PATH}/${PREFERENCE_ID}" \
   --include \
-  --request PATCH \
-  --header "Authorization: Token token=${TOKEN}" \
+  --request DELETE \
   --header "Content-Type: application/json" \
-  --data '{
-  "preference": {
-    "location": "'"${LOCATION}"'",
-    "search_radius": "'"${SEARCH_RADIUS}"'"
-  }
-}'
+  --header "Authorization: Token token=$TOKEN"
 
 echo

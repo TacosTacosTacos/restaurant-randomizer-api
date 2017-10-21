a2d88f469cc899b0bf106260561503ae44597743
tacostacostacos <david.epstein@libertymutual.com>
2017-10-21 16:00:11 -0400

    Created script for testing herouku connection

---
+++ scripts/auth/herouku-sign-up.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+EMAIL="TestUser1333323@gmail.com"
+PASSWORD="asdf"
+API="${API_ORIGIN:-https://restaurant-randomizer-api.herokuapp.com}"
+URL_PATH="/sign-up"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
